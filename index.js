35fab5e68bb22c28374770e3bd537751f196627a
Priyallohar <128009939+Priyallohar@users.noreply.github.com>
2023-05-03 11:20:34 +0000

    Add chalk style in code

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 let readlineSync = require("readline-sync");
+const chalk = require('chalk');
+
 
 let Score = 0;
-let userScores = [];
-let userName = readlineSync.question("What's your name? ");
-
-function joiner() {
-  console.log("Welcome, " + userName + ", to the Quiz of Potterhead!");
+let otherScores = {
+  name: "Mukul",
+  points: 5,
 }
+;
+let userName = readlineSync.question(chalk.cyanBright("What's your name? "));
+
+console.log(chalk.magentaBright("Welcome, " +userName+ ", to the Quiz of Potterhead!"));
+console.log(chalk.yellow("-----------------------------"))
+
 
 function playGame(question, answer) {
   let userAnswer = readlineSync.question(question);
@@ -14,26 +20,22 @@ function playGame(question, answer) {
     console.log("Yoo!! You got it right.");
     Score++;
   } else {
-    console.log("You got it wrong.");
+    console.log("Oops!! You got it wrong.");
   }
+  console.log(chalk.greenBright("Your current score: ", Score));
+  console.log(chalk.yellow("-----------------------------"))
 }
 
-joiner();
 
-let potterQuestion = [  {    question: "Who wanted to kill Harry Potter? ",    answer: "Voldemort"  },  {    question: "Who killed Sirius Black? ",    answer: "Bellatrix"  },  {    question: "Who killed Dumbledore? ",    answer: "Severus Snape"  },  {    question: "Cedric was captain of which team? ",    answer: "Hufflepuff"  },  {    question: "Who killed Bellatrix? ",    answer: "Molly"  },  {    question: "Which Weasley brother died in battle? ",    answer: "Fred"  }];
+let potterQuestion = [{ question: "Who wanted to kill Harry Potter? ", answer: "Voldemort" }, { question: "Who killed Sirius Black? ", answer: "Bellatrix" }, { question: "Who killed Dumbledore? ", answer: "Severus Snape" }, { question: "Cedric was captain of which team? ", answer: "Hufflepuff" }, { question: "Who killed Bellatrix? ", answer: "Molly" }, { question: "Which Weasley brother died in battle? ", answer: "Fred" }];
 
 for (let i = 0; i < potterQuestion.length; i++) {
   let currentQuestions = potterQuestion[i];
   playGame(currentQuestions.question, currentQuestions.answer);
 }
 
-function addUserScore(name, score) {
-  let UserPoints = {
-    name: name,
-    score: score
-  };
-  userScores.push(UserPoints);
-}
-addUserScore(userName, Score);
+console.log(chalk.magentaBright(`YAY! Your Total Score Is ${Score}. Well Done Potterhead`))
 
-console.log(`YAY! your score is ${Score}`)
+console.log(chalk.yellow("-----------------------------"))
+
+console.log( chalk.red(`Others Score: ${otherScores.name} secure ${otherScores.points}`))
